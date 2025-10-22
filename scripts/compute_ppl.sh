230a631da2aae2ae7e6dac2dc41d0c0da6a45019
baohaoliao <liaobaohao@gmail.com>
2025-10-22 16:52:09 +0200

    ppl script

--- scripts/compute_ppl.sh
+++ scripts/compute_ppl.sh
@@ -2,6 +2,9 @@
 
 MODEL=/mnt/nushare2/data/baliao/PLLMs/Gen-Verse/TraDo-4B-Instruct
 DATA=/mnt/nushare2/data/baliao/dlm/data/hendrydong/test.json
+SAVE_DIR=/mnt/nushare2/data/baliao/dlm/00_start/ppl_results/Gen-Verse/TraDo-4B-Instruct
+
+mkdir -p ${SAVE_DIR}
 
 for block_len in 4; do
     echo "Evaluating block_length=${block_len}"
@@ -11,7 +14,8 @@ for block_len in 4; do
         --dataset_path ${DATA} \
         --block_length ${block_len} \
         --batch_size 16 \
-        --max_samples 100 \
-        --max_length 4096 \
-        --tensor_parallel_size 1
+        --max_samples 1 \
+        --max_length 2048 \
+        --tensor_parallel_size 1 \
+        --output_file ${SAVE_DIR}/blocklen_${block_len}.json
 done
